4a01935876c696bdcb06d967819513cabb5a9f36
Daniel Marzini <daniel@marzini.net>
2015-02-20 15:32:03 +0100

    fix

--- deploy/pull_from_remote_subversion.sh
+++ deploy/pull_from_remote_subversion.sh
@@ -2,5 +2,5 @@
 
 apt-get update && apt-get -y install subversion
 
-svn --non-interactive --username $USER --password $PWD co $REPO_URL $DEST
+svn --non-interactive --username $USER --password $PASSWORD co $REPO_URL $DEST
 exit 0
