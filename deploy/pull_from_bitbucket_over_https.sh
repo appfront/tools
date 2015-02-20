2eeee1f54826e8f8c1911f362073f91cd381e24b
Daniel Marzini <daniel@marzini.net>
2015-02-20 11:47:51 +0100

    f

--- deploy/pull_from_bitbucket_over_https.sh
+++ deploy/pull_from_bitbucket_over_https.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-git clone https://$BITBUCKER_USER:$BITBUCKET_PWD@bitbucket.org/$REPO_URL $DEST
+git clone https://$BITBUCKET_USER:$BITBUCKET_PWD@bitbucket.org/$REPO_URL $DEST
 rm -rf $DEST/.git
 exit 0
