d89424e060603d66816bddd8219144d17fe63da9
Daniel Marzini <daniel@marzini.net>
2015-02-25 20:16:56 +0100

    compile_node_app without APP_NAME

---
+++ compile/compile_node_app_plain.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+node --version
+
+npm install -g bower
+npm install -g grunt
+npm install -g grunt-cli
+
+grunt --version
+bower --version
+
+cd $DEST
+
+npm install
+bower install --allow-root | xargs echo
+#npm test
+grunt $BUILD_VERSION
+mv $DEST/dist /opt
+rm -rf /var/www/*
+mv /opt/dist/* /var/www/
